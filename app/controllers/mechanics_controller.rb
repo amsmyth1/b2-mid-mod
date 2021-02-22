109af471e0ecd01271109b31662ff651929e2577
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-22 13:00:48 -0500

    Create update method in controller, remove empty line in routes, all tests passing

--- app/controllers/mechanics_controller.rb
+++ app/controllers/mechanics_controller.rb
@@ -7,4 +7,12 @@ class MechanicsController < ApplicationController
     @mechanic = Mechanic.find(params[:id])
     @rides = @mechanic.rides.open_rides.order(thrill_rating: :desc)
   end
+
+  def update
+    mechanic = Mechanic.find(params[:id])
+    new_ride = Ride.find(params[:ride_id])
+    mechanic.rides << new_ride
+
+    redirect_to "/mechanics/#{mechanic.id}"
+  end
 end
