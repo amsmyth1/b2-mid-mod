8aed2f36cf473d2f58f687fe71ef4f06a07b1cce
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-22 12:01:46 -0500

    Create test for scope and put scope in html view

--- spec/models/mechanic_spec.rb
+++ spec/models/mechanic_spec.rb
@@ -1,8 +1,26 @@
 require 'rails_helper'
 
 describe Mechanic, type: :model do
+  before :each do
+    @jane = Mechanic.create(name: "Jane Goodwin", years_experience: 10)
+    @jack = Mechanic.create(name: "Jack Yarn", years_experience: 8)
+    @mike = Mechanic.create(name: "Mike Bigalo", years_experience: 5)
+    @meg = Mechanic.create(name: "Meg Shell", years_experience: 15)
+    @bob = Mechanic.create(name: "Bob Bobberan", years_experience: 30)
+    @betty = Mechanic.create(name: "Betty Sweaty", years_experience: 1)
+
+    @average_years_experience = 11.5
+  end
+
   describe "relationships" do
     it { should have_many :ride_mechanics}
     it { should have_many(:rides).through(:ride_mechanics)}
+  end
+
+  describe "scopes" do
+    it "should return the average_years_experience" do
+
+      expect(Mechanic.average_years_experience).to eq(@average_years_experience)
+    end
   end
 end
