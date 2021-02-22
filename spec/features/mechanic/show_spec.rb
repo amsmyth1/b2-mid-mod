44f6a3ad779785159192a3ce26d8418d5fd2ed16
Alexa Morales Smyth <alexamsmyth@gmail.com>
2021-02-22 12:21:15 -0500

    Create show tests for user story 2

---
+++ spec/features/mechanic/show_spec.rb
@@ -0,0 +1,44 @@
+require 'rails_helper'
+
+RSpec.describe "mechanic's show page", type: :feature do
+  before :each do
+    @jane = Mechanic.create(name: "Jane Goodwin", years_experience: 10)
+    @kingda_ka = Ride.create(name: "Kingda Ka", thrill_rating: 10, open: false)
+    @nitro = Ride.create(name: "Nitro", thrill_rating: 8, open: true)
+    @toro = Ride.create(name: "El Toro", thrill_rating: 6, open: true)
+    @batman = Ride.create(name: "Batman The Ride", thrill_rating: 7, open: true)
+
+    @jane.rides << @kingda_ka
+    @jane.rides << @nitro
+    @jane.rides << @toro
+    @jane.rides << @batman
+  end
+
+  describe "can display all mechanic information" do
+    it "shows their name, years of experience, and the names of all rides they’re working on" do
+      visit "/mechanics/#{@jane.id}"
+
+      expect(page).to have_content(@jane.name)
+      expect(page).to have_content(@jane.years_experience)
+      expect(page).to have_content(@nitro.name)
+      expect(page).to have_content(@toro.name)
+      expect(page).to have_content(@batman.name)
+    end
+
+    it "shows only rides that are open" do
+      visit "/mechanics/#{@jane.id}"
+
+      expect(page).to_not have_content(@kingda_ka.name)
+    end
+
+    it "lists all rides by most thrilling first" do
+      visit "/mechanics/#{@jane.id}"
+
+      within ".mechanic_rides" do
+        expect(@nitro.name).to_appear before(@batman.name)
+        expect(@nitro.name).to_appear before(@toro.name)
+        expect(@batman.name).to_appear before(@toro.name)
+      end 
+    end
+  end
+end
